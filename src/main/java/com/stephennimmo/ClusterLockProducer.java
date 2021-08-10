7f2a3fc54a59a4148ed936def698a127f6468cbd
Stephen Nimmo <stephennimmo@gmail.com>
2021-08-10 10:49:23 -0500

    Added namspace to deployment for lease

--- src/main/java/com/stephennimmo/ClusterLockProducer.java
+++ src/main/java/com/stephennimmo/ClusterLockProducer.java
@@ -6,14 +6,19 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.cluster.CamelClusterService;
 import org.apache.camel.component.file.cluster.FileLockClusterService;
 import org.apache.camel.component.kubernetes.cluster.KubernetesClusterService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class ClusterLockProducer {
+
+    @ConfigProperty(name = "namespace")
+    Optional<String> namespace;
 
     @Produces
     @UnlessBuildProfile("prod")
@@ -30,7 +35,9 @@ public class ClusterLockProducer {
     @IfBuildProfile("prod")
     public CamelClusterService kubernetesClusterService(CamelContext camelContext) {
         KubernetesClusterService service = new KubernetesClusterService();
-        service.setCamelContext(camelContext);
+        if (namespace.isPresent()){
+            service.setKubernetesNamespace(namespace.get());
+        }
         return service;
     }
 
--- src/main/java/com/stephennimmo/beanlogger/BeanLoggerLeadershipListener.java
+++ src/main/java/com/stephennimmo/beanlogger/BeanLoggerLeadershipListener.java
@@ -5,6 +5,7 @@ import io.quarkus.runtime.StartupEvent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.cluster.CamelClusterEventListener;
 import org.apache.camel.cluster.CamelClusterService;
+import org.apache.camel.component.kubernetes.cluster.KubernetesClusterService;
 import org.apache.camel.support.cluster.ClusterServiceHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,11 @@ public class BeanLoggerLeadershipListener {
 
     void onStart(@Observes StartupEvent ev) throws Exception {
         CamelClusterService camelClusterService = ClusterServiceHelper.lookupService(camelContext).orElseThrow(() -> new RuntimeException("Unable to lookupService for CamelClusterService"));
+        if (camelClusterService instanceof KubernetesClusterService) {
+            KubernetesClusterService kcs = (KubernetesClusterService) camelClusterService;
+            LOGGER.info("KubernetesClusterService: LeaseResourceType={}, KubernetesNamespace={}, KubernetesResourceName={}, MasterUrl={}",
+                    kcs.getLeaseResourceType().name(), kcs.getKubernetesNamespace(), kcs.getKubernetesResourceName(), kcs.getMasterUrl());
+        }
         camelClusterService.getView("bean-logger-ns").addEventListener((CamelClusterEventListener.Leadership) (view, leader) -> {
             LOGGER.info("LeadershipEvent[bean-logger-ns]: {}", leader);
             boolean weAreLeader = leader.isPresent() && leader.get().isLocal();
